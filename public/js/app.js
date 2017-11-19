fb5289d244ce6c5f01bc6b816234a537d805a766
Yagnik Vadher <vadheryagnik111@gmail.com>
2017-11-19 01:42:18 -0800

    Added a google map API key

--- public/js/app.js
+++ public/js/app.js
@@ -2,7 +2,7 @@
 //Angular App Module and Controller
 var mapApp = angular.module('mapApp', ['ngDialog']);
 mapApp.controller('MapController', function ($scope, $http, ngDialog) {
-    
+
     $scope.openTopics = function() {
         ngDialog.openConfirm({template: 'modal.html',
             className: 'ngdialog-theme-default',
@@ -18,7 +18,7 @@ mapApp.controller('MapController', function ($scope, $http, ngDialog) {
 	};
 
     var createMarker = function (info, i){
-        
+
         var marker = new google.maps.Marker({
             map: $scope.map,
             position: new google.maps.LatLng(info.lat, info.long),
@@ -31,11 +31,11 @@ mapApp.controller('MapController', function ($scope, $http, ngDialog) {
             activeUsers += info.topics[i].activePeople;
         }
         marker.content = '<div class="infoWindowContent">' + info.desc + '<br />Total topics: ' + info.topics.length + ' !!<br>Active people <i class="fa fa-user" aria-hidden="true"/> : ' + activeUsers +  ' </div>';
-        
+
         google.maps.event.addListener(marker, 'click', function(){
             $scope.infoWindow.setContent('<h3>' + marker.title + '</h3>' +  marker.content);
             $scope.infoWindow.open($scope.map, marker);
-            
+
             if (marker.getAnimation() !== null) {
             marker.setAnimation(null);
             } else {
@@ -59,18 +59,18 @@ mapApp.controller('MapController', function ($scope, $http, ngDialog) {
 
         cityCircle.setMap($scope.map);
         $scope.markers[i] = marker;
-    }  
+    }
 
     function getDistanceFromLatLon(lat1,lon1,lat2,lon2) {
         var R = 6371; // Radius of the earth in km
         var dLat = deg2rad(lat2-lat1);  // deg2rad below
-        var dLon = deg2rad(lon2-lon1); 
-        var a = 
+        var dLon = deg2rad(lon2-lon1);
+        var a =
           Math.sin(dLat/2) * Math.sin(dLat/2) +
-          Math.cos(deg2rad(lat1)) * Math.cos(deg2rad(lat2)) * 
+          Math.cos(deg2rad(lat1)) * Math.cos(deg2rad(lat2)) *
           Math.sin(dLon/2) * Math.sin(dLon/2)
-          ; 
-        var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a)); 
+          ;
+        var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
         var d = R * c; // Distance in km
         return d*1000;
       };
@@ -78,22 +78,22 @@ mapApp.controller('MapController', function ($scope, $http, ngDialog) {
         return deg * (Math.PI/180)
     };
 
-    // setInterval(function(){ 
+    // setInterval(function(){
     //     for (i = $scope.totalData; i < $scope.data.length; i++){
     //         createMarker($scope.data[i]);
     //     }
     //  }, 3000);
 
-    $http.get('js/data.json').then(function(response){
+    $http.get('./js/data.json').then(function(response){
         $scope.data = response.data;
         $scope.totalData = $scope.data.length;
         var h = window.innerHeight;
         var w = window.innerWidth;
-       
+
         $("#search").width(w/3- 35);
         $("#map").height(h - 100);
         $(".pre-scrollable").css('max-height', h - 350 );
-    
+
         var location = {lat: 49.2827, lng: -123.1207};
         var mapOptions = {
             zoom: 16,
@@ -105,16 +105,16 @@ mapApp.controller('MapController', function ($scope, $http, ngDialog) {
         $scope.map = new google.maps.Map(document.getElementById('map'), mapOptions);
         var autocomplete = new google.maps.places.Autocomplete($scope.inputLocation);
         $scope.markers = [];
-        
+
         $scope.infoWindow = new google.maps.InfoWindow();
-        
+
         for (i = 0; i < $scope.data.length; i++){
             createMarker($scope.data[i], i);
         }
 
         //Listen to zoom change to that we can remove radius zone when clustering happens
         $scope.map.addListener('zoom_changed', function(){
-            
+
         });
 
         var optionForCluster = {imagePath : 'images/m' };
@@ -127,7 +127,7 @@ mapApp.controller('MapController', function ($scope, $http, ngDialog) {
 
         $scope.addTopic = function(){
             if ($scope.topicTitle && $scope.topicLocation && $scope.topicDes){
-            
+
                 axios.get('https://maps.googleapis.com/maps/api/geocode/json',{
                     params:{
                       address: $scope.topicLocation,
@@ -136,13 +136,13 @@ mapApp.controller('MapController', function ($scope, $http, ngDialog) {
                   })
                   .then(function(response){
                     // Log full response
-        
+
                     // Formatted Address
                     var formattedAddress = response.data.results[0].formatted_address;
                      // Geometry
                     var lat = response.data.results[0].geometry.location.lat;
                     var lng = response.data.results[0].geometry.location.lng;
-                    
+
                     $scope.map.panTo({lat, lng});
                     var isInData = false;
                     var indexObj = 0;
@@ -173,13 +173,13 @@ mapApp.controller('MapController', function ($scope, $http, ngDialog) {
                             "topics" : [{ title : $scope.topicTitle, activePeople : 1}]
                         }
                         $scope.data.push(obj);
-                        createMarker(obj, $scope.data.length);  
+                        createMarker(obj, $scope.data.length);
                     }
-                    
+
                   });
             }
         }
 
     });
-    
+
 });
